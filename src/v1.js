0bbd077c1bb2871e321e881a6cf49f466e3b6441
Legend-of-iPhoenix <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-01-26 11:55:04 -0500

    v1 (source)

---
+++ src/v1.js
@@ -0,0 +1,26 @@
+var $$$ = new (function() {
+	this.get = function(s) {
+		return document.querySelectorAll(s);
+	}
+	this.keyWithValue = function(j, v) {
+		return Object.keys(j).find(key => j[key] === v);
+	}
+	this.append = function(d_s,t,flags){
+		var nL=flags.newline||flags.n;
+		var m=flags.mono||flags.m;
+		var i=flags.italic||flags.i;
+		var b=flags.bold||flags.b;
+		var u=flags.underline||flags.u;
+		t=this.cleanse(t);
+		t=m?"<pre>"+t+"</pre>":t;
+		t=i?"<em>"+t+"</em>":t;
+		t=b?"<strong>"+t+"</strong>":t;
+		t=u?"<u>"+t+"</u>":t;
+		d_s.innerHTML += nL?"\n"+t:t;
+	}
+	this.cleanse = function(t) {
+		var n = document.createElement("p");
+  	n.innerText = t;
+  	return n.innerHTML;
+	}
+})();
